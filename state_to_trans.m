ad31b86e512d079fb1cab8a2b9aebaac5fca00c3
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2024-10-14 20:11:06 +0200

    Add files via upload

---
+++ state_to_trans.m
@@ -0,0 +1,24 @@
+% https://youtu.be/CwB8FzXWvgY
+syms s
+A = [0,1,0,0;0,-703,-5.2,14.8;0,0,0,1;0,3135,58.4,-65.8];
+B = [0;33.2;0;-14.8];
+C = [0,0,1,0];
+D = 0;
+% Trans
+%{
+id = eye(4);
+mat = id*s-A;
+invers = inv(mat);
+trans = C*invers*B;
+Trans = ss2tf(A,B,C,D)
+%}
+sys = ss(A,B,C,D);
+sys_as_tf = tf(sys);
+% Poles and zeros
+poles = pole(sys_as_tf);
+zeros = zero(sys_as_tf);
+% 3 eigenvalues
+eigen = eig(A);
+
+% Finding L vector
+L=place(A,B,[-0.1 -500 -5 -7.5]);
